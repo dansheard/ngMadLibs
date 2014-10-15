6a0c59156a96dbb0fb32652456fdedd2d06dc91e
Dan Sheard <dan.c.sheard@gmail.com>
2014-10-15 21:15:11 +0100

    Initial refactor of mad libs

---
+++ assets/js/MadLibs.js
@@ -0,0 +1,12 @@
+angular.module('MadLibs', [])
+			 	.controller('datactrl', function($scope) {
+					$scope.model = {female_name: '',
+													job_title: '',
+													tedious_task: '',
+													dirty_task: '',
+													celebrity: '',
+													useless_skill: '',
+													obnoxious_celebrity: '',
+													huge_number: '',
+													adjective: '' }
+});
