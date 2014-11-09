57908b0bb9c2ef4911fdf3168ea9f759ff36de73
Dan Sheard <dan.c.sheard@gmail.com>
2014-11-09 22:58:12 +0000

    Finish functionality and refactor

--- assets/js/MadLibs.js
+++ assets/js/MadLibs.js
@@ -1,12 +1,31 @@
-angular.module('MadLibs', [])
-			 	.controller('datactrl', function($scope) {
-					$scope.model = {person_name: '',
-													job_title: '',
-													tedious_task: '',
-													dirty_task: '',
-													celebrity: '',
-													useless_skill: '',
-													obnoxious_celebrity: '',
-													huge_number: '',
-													adjective: '' }
-});
+angular.module('MadLibs', ['ngMessages'])
+  .controller('FormCtrl', ['$scope', function($scope) {
+
+    $scope.submit = function(user) {
+      if ( $scope.madLibsForm.$valid ) {
+      	$scope.showMadLibs = true;
+			} else {
+				$scope.showMadLibs = false;
+			}
+    };
+
+    $scope.reset = function() {
+	    $scope.model = {};
+	    $scope.madLibsForm.$setPristine();
+	    $scope.submitted = false;
+	    $scope.showMadLibs = false;
+	  };
+
+    $scope.model = {
+			personName: '',
+			jobTitle: '',
+			tediousTask: '',
+			dirtyTask: '',
+			celebrity: '',
+			uselessSkill: '',
+			obnoxiousCelebrity: '',
+			hugeNumber: '',
+			adjective: '' 
+		}
+
+  }]);
